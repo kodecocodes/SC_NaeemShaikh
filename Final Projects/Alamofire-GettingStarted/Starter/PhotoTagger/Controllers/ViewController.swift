0182088b2bee3a4e4b098380d3134d8cfb3c3db0
Naeem Shaikh <naeemshaikh90@yahoo.co.in>
2017-12-14 20:44:33 +0530

    Split Up Final Sample Projects

---
+++ Final Projects/Alamofire-GettingStarted/Starter/PhotoTagger/Controllers/ViewController.swift
@@ -0,0 +1,89 @@
+/// Copyright (c) 2017 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  // MARK: - IBOutlets
+  @IBOutlet var takePictureButton: UIButton!
+  @IBOutlet var imageView: UIImageView!
+  @IBOutlet var progressView: UIProgressView!
+  @IBOutlet var activityIndicatorView: UIActivityIndicatorView!
+
+  // MARK: - View Life Cycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    guard !UIImagePickerController.isSourceTypeAvailable(.camera) else { return }
+
+    takePictureButton.setTitle("Select Photo", for: .normal)
+  }
+
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(animated)
+
+    imageView.image = nil
+  }
+
+  // MARK: - IBActions
+  @IBAction func takePicture(_ sender: UIButton) {
+    let picker = UIImagePickerController()
+    picker.delegate = self
+    picker.allowsEditing = false
+
+    if UIImagePickerController.isSourceTypeAvailable(.camera) {
+      picker.sourceType = .camera
+    } else {
+      picker.sourceType = .photoLibrary
+      picker.modalPresentationStyle = .fullScreen
+    }
+
+    present(picker, animated: true)
+  }
+}
+
+// MARK: - UIImagePickerControllerDelegate
+extension ViewController: UIImagePickerControllerDelegate {
+
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
+    guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+      print("Info did not have the required UIImage for the Original Image")
+      dismiss(animated: true)
+      return
+    }
+
+    imageView.image = image
+    
+    dismiss(animated: true)
+  }
+}
+
+// MARK: - UINavigationControllerDelegate
+extension ViewController: UINavigationControllerDelegate {
+}
